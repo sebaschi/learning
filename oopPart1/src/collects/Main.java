1d70b17d1a3bb9cb34d7edba56dabbeaee995850
seb <seb@192.168.1.5>
2020-12-01 22:58:19 +0100

    first steps in java collections

---
+++ oopPart1/src/collects/Main.java
@@ -0,0 +1,25 @@
+package collects;
+
+public class Main {
+	
+	public static void print(String s) {
+		System.out.println(s);
+	}
+
+	public static void main(String[] args) {
+		Theatre corso = new Theatre("Corso", 8, 12);
+		corso.getSeats();
+		if(corso.reserveSeat("H11")) {
+			print("please pay ");
+		} else {
+			print("sorry seat is taken");
+		}
+		if(corso.reserveSeat("H11")) {
+			print("please pay ");
+		} else {
+			print("sorry seat is taken");
+		}
+	
+	}
+
+}
